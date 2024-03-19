ba0ccfdd56bf2e5398a08ca18e8cf271f3bd0bf0
mantlei <marco.paeldon@student.ateneo.edu>
2024-03-19 19:10:41 +0800

    just changed BG color in the homepage

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -50,7 +50,7 @@ const Homepage: React.FC = () => {
   
 
   return (
-    <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
+    <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center", background: "#2a475e" }}>
       {loading ? (
         <Spin size="large" />
       ) : (
