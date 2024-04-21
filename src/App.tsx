ada52c4960302821b933c39a5c70c94ad70e7be3
Deokhyun Lee <deok.lee@obf.ateneo.edu>
2024-04-21 20:07:50 +0800

    game recommedation implementation is added

--- src/App.tsx
+++ src/App.tsx
@@ -86,7 +86,7 @@ const App: React.FC = () => {
                 />
                 <Route
                   path="/recommendations"
-                  element={<GameRecommendation recommendations={[]} />}
+                  element={<GameRecommendation/>}
                 />
                   <Route
                   path="/login"
--- src/components/GameRecommendation.tsx
+++ src/components/GameRecommendation.tsx
@@ -1,23 +1,112 @@
-import React from 'react';
-import { Card } from 'antd';
+import React, { useState, useEffect } from "react";
+import { Card, Spin, Tag } from "antd";
+import { Game } from "../interfaces/game";
+import { Link } from "react-router-dom";
 
-interface GameRecommendation {
-  name: string;
-  description: string;
-}
+const GameRecommendation: React.FC = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
-const GameRecommendation: React.FC<{ recommendations: GameRecommendation[] }> = ({
-  recommendations,
-}) => {
+  const fetchGames = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/games", {
+        method: "GET",
+      });
+      if (!response.ok) {
+        throw new Error("Failed to fetch games");
+      }
+
+      const data = await response.json();
+
+      let gamesArray = Object.keys(data).map((key) => data[key]);
+
+    // Randomly select 4 games
+      gamesArray.sort(() => Math.random() - 0.5);
+      gamesArray = gamesArray.slice(0, 5);
+      
+      setGames(gamesArray);
+      console.log("GAMES: ", gamesArray);
+
+      setLoading(false);
+    } catch (error: any) {
+      console.error("Error fetching games:", error.message);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchGames();
+  }, []);
+
+  const formatPrice = (price: number) => {
+    const formatter = new Intl.NumberFormat("en-PH", {
+      style: "currency",
+      currency: "PHP",
+    });
+    return formatter.format(price);
+  };
+
+  const formatDate = (dateString: string) => {
+    const options: Intl.DateTimeFormatOptions = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    return new Date(dateString).toLocaleDateString("en-US", options);
+  };
+
+  const calculateWaitTime = (releaseDate: string) => {
+    const currentDate = new Date();
+    const releaseDateObj = new Date(releaseDate);
+    const differenceInDays = Math.floor(
+      (releaseDateObj.getTime() - currentDate.getTime()) / (1000 * 3600 * 24)
+    );
+    return differenceInDays;
+  };
+
   return (
-    <>
-      {/* {recommendations.map((game, index) => (
-        <Card key={index} title={game.name}>
-          <p>{game.description}</p>
-        </Card>
-      ))} */}
-      <p>placeholder hehe, you may put your recommendation here</p>
-    </>
+    <div>
+    <h2>Your Recommendations by W2b 😍</h2>
+    <div
+      style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
+    >
+      
+      {loading ? (
+        <Spin size="large" />
+      ) : (
+        games.map((game, index) => (
+          <Link key={index} to={`/game/${game._id}`}>
+         
+          <Card
+            key={index}
+            title={game.game_name}
+            style={{ width: 300, margin: "20px", borderRadius: 10 }}
+            hoverable
+            cover={
+              <img
+                alt={game.game_name}
+                src={game.gameImageUrl}
+                style={{ borderRadius: "0 0 0 0", maxHeight: 200 }}
+              />
+            }
+          >
+            <p style={{ fontWeight: "bold", marginBottom: 5 }}>Release Date:</p>
+            <p>{formatDate(game.date)}</p>
+            <p style={{ fontWeight: "bold", marginBottom: 5 }}>Price:</p>
+            <p>{formatPrice(parseFloat(game.price))}</p>
+            <p style={{ fontWeight: "bold", marginBottom: 5 }}>Wait Time:</p>
+
+            {parseFloat(game.time) >= 30 ? (
+              <Tag color="red">{game.time} days, DO NOT BUY</Tag>
+            ) : (
+              <Tag color="orange">{game.time} days, hold</Tag>
+            )}
+          </Card>
+          </Link>
+        ))
+      )}
+    </div>
+    </div>
   );
 };
 
